6ee64b02ed57585944839f9f64fa429a4b5251c2
Maria Clara <166544830+clarabarretoo@users.noreply.github.com>
2024-04-11 21:18:56 +0000

    Update Cliente.java

--- Cliente.java
+++ Cliente.java
@@ -1,27 +1,28 @@
-public Cliente (String n,int c, String d) {
-		this.nome= n;
-		this.cpf= c;
-		this.dataNascimento = d;
-	}
-	public void setNome (String n) {
-		this.nome = n;
-	}
-	public void setCpf (int c) {
-		this.cpf = c;
-	}
-	public void setdataNascimento ( String d) {
-		this.dataNascimento = d;
-	}
-	public String getNome () {
-		return nome;
-	}
-	public int getCpf() {
-		return cpf;
-	}
-	public String getDataNascimento() {
-		return dataNascimento;
-	}
-	public String toString () {
-		return "nome: " + nome + "\ncpf: " + cpf +"\ndataNascimento: " + dataNascimento;
-	}
-}
+ public class Cliente{
+	  private String nome;
+	  private String cpf;
+	  private Data dataNascimento;
+	  
+	  public Cliente(String nome, String cpf, Data datanascimento) {
+	        this.nome = nome;
+	        this.cpf = cpf;
+	        this.data= dataNascimento;     
+	  } 
+	   public String getNome() {
+	        return nome;
+	    }
+	   
+	   public String getCpf() {
+	        return cpf;
+	    }
+	   
+	   public void setNome(String nome) {
+	      this.nome=nome;
+	   }
+	   
+	   public void setCpf(String cpf) {
+		      this.cpf=cpf;
+       }
+	   public void setDataNascimento(Data d) {
+		      this.dataNascimento=d;
+	   }

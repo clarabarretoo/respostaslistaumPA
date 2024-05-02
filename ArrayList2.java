d695a86c5fd542ef270d30f04219f17fe2172207
Maria Clara <166544830+clarabarretoo@users.noreply.github.com>
2024-05-02 20:40:33 +0000

    Create ArrayList2.java

---
+++ ArrayList2.java
@@ -0,0 +1,15 @@
+package exemploArrayList;
+
+import java.util.ArrayList;
+
+public class ExemploArrayList2 {
+	public static void main (String[] args) {
+		ArrayList<Integer> numeros = new ArrayList<Integer>();
+		
+		numeros.add(2600);
+		numeros.add(18);
+		numeros.add(2006);
+		
+		System.out.println(numeros);
+	}
+}

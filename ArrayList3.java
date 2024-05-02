4159227c0f772185c98355ad623b93965c6b0703
Maria Clara <166544830+clarabarretoo@users.noreply.github.com>
2024-05-02 20:39:10 +0000

    Create ArrayList3.java

---
+++ ArrayList3.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+
+	public class ExemploArrayList3 {
+		public static void main (String[] args) {
+			ArrayList<Double> numeros = new ArrayList<Double>();
+			
+			numeros.add(28.12);
+			numeros.add(1.8);
+			numeros.add(2.006);
+			
+			System.out.println(numeros);
+		}
+	}
